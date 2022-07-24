17801219e76c96cc665a02b7a2317e956f3bdc96
sdinushka <109921334+sdinushka@users.noreply.github.com>
2022-07-25 01:49:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.owner = ['94787811464']
+global.premium = ['94787811464']
+global.ownernomer = '94787811464'
+global.ownername = 'MR.SHENU'
+global.botname = 'MR.SHENU BOT'
+global.footer = '©MR.SHENU.'
+global.ig = 
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.sc =
+global.myweb = 
+global.packname = 'MR.SHENU'
+global.author = 'MR.SHENUWA'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
